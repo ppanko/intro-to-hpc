7720f1adb5915eae79e43368cee0fd0e84e8e307
ppanko <pavelppanko@gmail.com>
2018-10-26 01:06:26 -0500

    Update run_testJob.R

--- testJob-basic/run_testJob.R
+++ testJob-basic/run_testJob.R
@@ -1,1 +1,14 @@
+#!/bin/sh
+#$ -V
+#$ -cwd
+#$ -S /bin/bash
+#$ -N testJob
+#$ -o $JOB_NAME.o$JOB_ID
+#$ -e $JOB_NAME.e$JOB_ID
+#$ -q omni
+#$ -P quanah
+#$ -pe sm 10
 
+module load intel R 
+
+Rscript 02_testJob-parallel.R
